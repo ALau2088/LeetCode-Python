a2bd1754b6edb99857a61fa24b8848ed284d4538
ALau2088 <alau12004@yahoo.com>
2019-11-01 11:17:28 -0700

    update 807

--- 807-Max Increase to Keep City Skyline.py
+++ 807-Max Increase to Keep City Skyline.py
@@ -1,5 +1,9 @@
 class Solution:
   def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int: # grid: , int: are optional function annotations per python3.6.
-    print(type(grid)) # <class 'list'>
-    row_maxes = [max(row) for row in grid]
-    col_maxes = [max(col) for col in zip(*grid)]
+    # print(type(grid)) # <class 'list'>
+    row_maxes = [max(row) for row in grid] # [8, 7, 9, 3]
+    col_maxes = [max(col) for col in zip(*grid)] # [9, 4, 8, 7]
+
+    return sum(min(row_maxes[r], col_maxes[c]) - val
+      for r, row in enumerate(grid)
+      for c, val in enumerate(row))
