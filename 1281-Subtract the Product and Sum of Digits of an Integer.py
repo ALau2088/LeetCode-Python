8b8637585515a54bd800e2c1646e9a25e7993a5d
ALau2088 <alau12004@yahoo.com>
2019-12-09 18:18:49 -0800

    update

--- 1281-Subtract the Product and Sum of Digits of an Integer.py
+++ 1281-Subtract the Product and Sum of Digits of an Integer.py
@@ -1,9 +1,20 @@
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
         number = str(n)
-        product = 1;
-        sum = 0;
+        product = 1
+        sum = 0
         for digit in number:
             product = product * int(digit)
             sum = sum + int(digit)
         return product - sum
+
+# Input: n = 234
+# Output: 15
+# Explanation:
+# Product of digits = 2 * 3 * 4 = 24
+# Sum of digits = 2 + 3 + 4 = 9
+# Result = 24 - 9 = 15
+
+
+Obj = Solution()
+print(Obj.subtractProductAndSum(234))
--- 807-Max Increase to Keep City Skyline.py
+++ 807-Max Increase to Keep City Skyline.py
@@ -1,9 +1,36 @@
 class Solution:
-  def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int: # grid: , int: are optional function annotations per python3.6.
-    # print(type(grid)) # <class 'list'>
-    row_maxes = [max(row) for row in grid] # [8, 7, 9, 3]
-    col_maxes = [max(col) for col in zip(*grid)] # [9, 4, 8, 7]
+    # grid: , int: are optional function annotations per python3.6.
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        # print(type(grid)) # <class 'list'>
+        row_maxes = [max(row) for row in grid]  # [8, 7, 9, 3]
+        # [9, 4, 8, 7] # The purpose of zip() is to map the similar index of multiple containers so that they can be used just using as single entity.
+        col_maxes = [max(col) for col in zip(*grid)]
 
-    return sum(min(row_maxes[r], col_maxes[c]) - val
-      for r, row in enumerate(grid)
-      for c, val in enumerate(row))
+        return sum(min(row_maxes[r], col_maxes[c]) - val
+                   for r, row in enumerate(grid)
+                   for c, val in enumerate(row))  # sum([res1,res2,...]); see List Comprehensions
+
+# Example:
+# Input: grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+# Output: 35
+# Explanation:
+# The grid is:
+# [ [3, 0, 8, 4],
+#   [2, 4, 5, 7],
+#   [9, 2, 6, 3],
+#   [0, 3, 1, 0] ]
+
+# The skyline viewed from top or bottom is: [9, 4, 8, 7]
+# The skyline viewed from left or right is: [8, 7, 9, 3]
+
+# The grid after increasing the height of buildings without affecting skylines is:
+
+# gridNew = [ [8, 4, 8, 7],
+#             [7, 4, 7, 7],
+#             [9, 4, 8, 7],
+#             [3, 3, 3, 3] ]
+
+
+Obj = Solution()
+print(Obj.maxIncreaseKeepingSkyline(
+    [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]))
